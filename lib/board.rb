3f3b267b6b1fc62999d38846a2de4132c1880459
Eric Belongea <eric.belongea@gmail.com>
2023-09-08 09:36:08 -0600

    Add board existance

---
+++ lib/board.rb
@@ -0,0 +1,9 @@
+class Board
+  attr_reader :board
+
+  def initialize
+    @board = 
+  end
+
+  def default_board
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,11 @@
+require './lib/cell'
+require './lib/board'
+
+RSpec.describe "#Board" do
+  before(:each) do
+    @board = Board.new
+  end
+  it 'exists' do
+    expect(@board).to be_instance_of Board
+  end
+end
