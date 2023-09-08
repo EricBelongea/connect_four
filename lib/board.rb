b0056e1c17d094ce8a293366ac6520b28d5d546a
Eric Belongea <eric.belongea@gmail.com>
2023-09-08 11:08:41 -0600

    Adds board layout

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,22 @@
+require_relative 'cell'
+
 class Board
   attr_reader :board
 
   def initialize
-    @board = 
+    @board = default_board
   end
 
   def default_board
+    board =[
+      [cell.new, cell.new, cell.new, cell.new, cell.new, cell.new, cell.new],
+      [cell.new, cell.new, cell.new, cell.new, cell.new, cell.new, cell.new],
+      [cell.new, cell.new, cell.new, cell.new, cell.new, cell.new, cell.new],
+      [cell.new, cell.new, cell.new, cell.new, cell.new, cell.new, cell.new],
+      [cell.new, cell.new, cell.new, cell.new, cell.new, cell.new, cell.new],
+      [cell.new, cell.new, cell.new, cell.new, cell.new, cell.new, cell.new],
+    ]
+    # puts "ABCDEFG"
+    # puts board
+  end
 end
