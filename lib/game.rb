057e3700808ffa56b1ee6dc7e5116dc8c207bcb8
Eric Belongea <eric.belongea@gmail.com>
2025-07-30 21:57:44 -0600

    Random updates to what the game displays. Could use a CLI overhaul

--- lib/game.rb
+++ lib/game.rb
@@ -13,14 +13,15 @@ class Game
     @start = nil
     @finish = nil
   end
-  puts "---------------------------------"
+  puts "--------------------------------------"
   puts "Welcome to our game, Quad Connect™"
-  puts "---------------------------------"
+  puts "--------------------------------------"
+  puts " "
   def play
     @board.default_board
     puts "Press 'P' to play, or press 'Q' to quit"
     wanna_play = gets.chomp.downcase
-    puts " "
+    # puts " "
     while wanna_play != 'p' && wanna_play != 'q'
       puts "Press 'P' to play, or press 'Q' to quit"
       wanna_play = gets.chomp.downcase
@@ -28,19 +29,31 @@ class Game
     if wanna_play == 'q'
       exit()
     end
-    puts "--------------------------------"
+    puts "-------------------------------------"
     puts "Great! Here's the board you'll be
           playing on."
+    puts "-------------------------------------"
     puts " "
     @board.display_board
-    puts "--------------------------------"
+    puts " "
+    puts "-------------------------------------"
     puts "Please enter your player name below"
-    puts "--------------------------------"
+    puts "-------------------------------------"
     name = gets.chomp.capitalize
     @player.assign_player_name(name)
+    puts "-------------------------------------"
+    puts " "
+    puts "Alrighty #{@player.name}."
+    puts " "
+    puts "-------------------------------------"
+    puts "Your piece will be the 'X'"
+    puts "-------------------------------------"
     @start = Time.now
     loop do
+      @board.display_board
+      puts "-------------------------------------"
       puts "#{@player.name}, it's now your turn!"
+      puts "-------------------------------------"
       @player.player_turn(@board)
       @board.display_board
       if @board.check_winner == 'x'
@@ -76,11 +89,16 @@ class Game
     total_time = @finish.to_i - @start.to_i
     puts "It took you #{total_time} seconds to play this game."
     # puts " "
-    puts "------------------------------"
-    puts "Don't disconnect, Quad Connect™!"
-    puts "------------------------------"
+    puts "------------------------------------"
+    puts "  Don't disconnect, Quad Connect™!"
+    puts "------------------------------------"
     puts "Play again?"
     puts " "
     play
   end
+end
+
+if __FILE__ == $0
+  game = Game.new
+  game.play
 end
